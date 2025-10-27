5ee7a9e383ae138e71a4985fa5786ba959fcffde
Mykola <koliazelman@gmail.com>
2025-10-27 14:34:31 +0100

    do all js tasks

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const login = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (login === "" || password === "") {
+        return alert("All form fields must be filled in");
+    }
+    
+    const userData = {
+        email: login,
+        password: password,
+    };
+    
+    console.log(userData);
+    
+    form.reset();
+})
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnColor = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const color = document.querySelector(".color")
+btnColor.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+})
+
+
